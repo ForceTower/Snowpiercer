5b01d221f05e190dedb79808e6e53970e2ef39bc
João Sena <joaopaulo761@gmail.com>
2020-11-30 15:03:14 -0300

    Return API do unchanged state

--- src/main/java/dev/forcetower/breaker/JOrchestra.java
+++ src/main/java/dev/forcetower/breaker/JOrchestra.java
@@ -14,8 +14,8 @@ public class JOrchestra {
         this.instance = instance;
     }
 
-    public Future<Outcome<Person>> login() {
-        var completable = new CompletableFuture<Outcome<Person>>();
+    public Future<Outcome<? extends Person>> login() {
+        var completable = new CompletableFuture<Outcome<? extends Person>>();
         instance.login(Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -26,8 +26,8 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<List<? extends Semester>>> semesters(long profileId) {
-        var completable = new CompletableFuture<Outcome<List<? extends Semester>>>();
+    public Future<Outcome<? extends List<? extends Semester>>> semesters(long profileId) {
+        var completable = new CompletableFuture<Outcome<? extends List<? extends Semester>>>();
         instance.semesters(profileId, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -38,8 +38,8 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<List<? extends SemesterInformation>>> allSemestersInformation(long profileId) {
-        var completable = new CompletableFuture<Outcome<List<? extends SemesterInformation>>>();
+    public Future<Outcome<? extends List<? extends SemesterInformation>>> allSemestersInformation(long profileId) {
+        var completable = new CompletableFuture<Outcome<? extends List<? extends SemesterInformation>>>();
         instance.allSemestersInformation(profileId, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -50,8 +50,8 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<List<? extends DisciplineData>>> grades(long profileId, long semesterId) {
-        var completable = new CompletableFuture<Outcome<List<? extends DisciplineData>>>();
+    public Future<Outcome<? extends List<? extends DisciplineData>>> grades(long profileId, long semesterId) {
+        var completable = new CompletableFuture<Outcome<? extends List<? extends DisciplineData>>>();
         instance.grades(profileId, semesterId, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -62,12 +62,12 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<MessagesDataPage>> messages(long profileId) {
+    public Future<Outcome<? extends MessagesDataPage>> messages(long profileId) {
         return messages(profileId, "");
     }
 
-    public Future<Outcome<MessagesDataPage>> messages(long profileId, String until) {
-        var completable = new CompletableFuture<Outcome<MessagesDataPage>>();
+    public Future<Outcome<? extends MessagesDataPage>> messages(long profileId, String until) {
+        var completable = new CompletableFuture<Outcome<? extends MessagesDataPage>>();
         instance.messages(profileId, until, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -78,8 +78,8 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<List<? extends Lecture>>> lectures(long classId, int limit, int offset) {
-        var completable = new CompletableFuture<Outcome<List<? extends Lecture>>>();
+    public Future<Outcome<? extends List<? extends Lecture>>> lectures(long classId, int limit, int offset) {
+        var completable = new CompletableFuture<Outcome<? extends List<? extends Lecture>>>();
         instance.lectures(classId, limit, offset, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
@@ -90,8 +90,8 @@ public class JOrchestra {
         return completable;
     }
 
-    public Future<Outcome<List<? extends LectureMissed>>> absences(long profileId,long classId, int limit, int offset) {
-        var completable = new CompletableFuture<Outcome<List<? extends LectureMissed>>>();
+    public Future<Outcome<? extends List<? extends LectureMissed>>> absences(long profileId,long classId, int limit, int offset) {
+        var completable = new CompletableFuture<Outcome<? extends List<? extends LectureMissed>>>();
         instance.absences(profileId, classId, limit, offset, Coroutines.getContinuation((outcome, throwable) -> {
             if (throwable != null) {
                 completable.completeExceptionally(throwable);
--- src/main/kotlin/dev/forcetower/breaker/result/Outcome.kt
+++ src/main/kotlin/dev/forcetower/breaker/result/Outcome.kt
@@ -1,14 +1,14 @@
 package dev.forcetower.breaker.result
 
-sealed class Outcome<T> {
+sealed class Outcome<out T> {
     data class Success<T>(val value: T) : Outcome<T>()
     data class Error<T>(val error: Throwable, val code: Int): Outcome<T>()
 
     val isSuccess: Boolean
         get() = this is Success
 
-    fun asSuccess() = this as Success<T>
-    fun asError() = this as Error<T>
+    fun asSuccess() = this as Success<out T>
+    fun asError() = this as Error<out T>
 
     companion object {
         fun <T> success(value: T): Success<T> {
