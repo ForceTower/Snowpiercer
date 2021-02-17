37ef8bb386db8beee9d9112c29d96f4117937ff9
João Sena <joaopaulo761@gmail.com>
2021-02-17 10:10:05 -0300

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 val groupName = "dev.forcetower.unes"
 val baseName = "snowpiercer"
-val artifactVersion = "1.1.2"
+val artifactVersion = "1.1.3"
 
 group = groupName
 version = artifactVersion
